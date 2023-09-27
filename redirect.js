578e8e53151cadc98d6e646ba9c904045c3bbd67
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-09-27 22:32:45 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@
 // @namespace    https://minecraft.fandom.com/
 // @version      2.0
 // @description  Little user script that redirects from minecraft.fandom.com (bad 👎) to minecraft.wiki (good 👍)
-// @author       Astral
+// @author       Astral (https://astralvrz.github.io/)
 // @match        *://minecraft.fandom.com/*
 // @icon         https://files.catbox.moe/y8i3zz.ico
 // @grant        none
