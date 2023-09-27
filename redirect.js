5b47dda8c3e4360ef2fc600f5cc530bbeddad6bf
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-09-27 22:06:15 +0200

    now actually works lol

--- redirect.js
+++ redirect.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1
 // @description  try to take over the world!
 // @author       Astral
-// @match        *minecraft.fandom.*
+// @match        *://minecraft.fandom.com/*
 // @icon         https://files.catbox.moe/y8i3zz.ico
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.location.href = "https://minecraft.wiki/";
+    window.onload = function() {
+        window.location.href = "https://minecraft.wiki/";
+    };
 })();
